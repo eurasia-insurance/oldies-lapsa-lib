edb63a9ac56ae59492812b875eafcd71e87c249b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-07-05 15:35:06 +0600

    new module CDI beans

---
+++ cdiutils/src/main/java/com/lapsa/lapsa/cdi/producers/LoggingProducer.java
@@ -0,0 +1,14 @@
+package com.lapsa.lapsa.cdi.producers;
+
+import java.util.logging.Logger;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+public class LoggingProducer {
+
+    @Produces
+    private Logger createLogger(InjectionPoint injectionPoint) {
+	return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
+}
