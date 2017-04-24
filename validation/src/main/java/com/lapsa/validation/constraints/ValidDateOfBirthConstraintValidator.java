e7ad30a35537fbecda27eabd171e057433a6a033
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-24 16:52:57 +0600

    code opt

--- validation/src/main/java/com/lapsa/validation/constraints/ValidDateOfBirthConstraintValidator.java
+++ validation/src/main/java/com/lapsa/validation/constraints/ValidDateOfBirthConstraintValidator.java
@@ -1,30 +1,14 @@
 package com.lapsa.validation.constraints;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 import com.lapsa.validation.ValidDateOfBirth;
 
 public class ValidDateOfBirthConstraintValidator extends ATemporalConstraintValidator<ValidDateOfBirth> {
 
     @Override
-    public void initialize(ValidDateOfBirth a) {
-    }
-
-    @Override
-    protected boolean validate(LocalDateTime value) {
-	throw unsupportedType(value.getClass());
-    }
-
-    @Override
     protected boolean validate(LocalDate value) {
 	return true;
-    }
-
-    @Override
-    protected boolean validate(LocalTime value) {
-	throw unsupportedType(value.getClass());
     }
 
 }
