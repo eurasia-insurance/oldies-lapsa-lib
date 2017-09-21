882d375c554cee26dfe22199bfb40e492741ec47
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-09-21 11:20:59 +0600

    bunddle supplier instead of locale supplier

--- commons/src/main/java/com/lapsa/commons/elements/LocalizedElement.java
+++ commons/src/main/java/com/lapsa/commons/elements/LocalizedElement.java
@@ -23,70 +23,65 @@ public interface LocalizedElement {
 
     //
 
-    default String displayName(final Supplier<Locale> localeSupplier) {
-	return displayName(localeSupplier.get());
+    default String displayName() {
+	return displayName(Locale.getDefault());
     }
 
-    default String displayNameFull(final Supplier<Locale> localeSupplier) {
-	return displayNameFull(localeSupplier.get());
+    default String displayNameFull() {
+	return displayNameFull(Locale.getDefault());
     }
 
-    default String displayNameShort(final Supplier<Locale> localeSupplier) {
-	return displayNameShort(localeSupplier.get());
+    default String displayNameShort() {
+	return displayNameShort(Locale.getDefault());
     }
 
     //
 
-    default String displayName() {
-	return displayName(Locale::getDefault);
+    default String displayName(Supplier<ResourceBundle> bundleSupplier) {
+	return Optional.ofNullable(bundleSupplier.get()).flatMap(v -> {
+	    try {
+		return Optional
+			.ofNullable(v.getString(String.format("%1$s.%2$s", this.getClass().getName(), name())));
+	    } catch (NullPointerException | MissingResourceException | ClassCastException e) {
+		return Optional.empty();
+	    }
+	}).orElseThrow(() -> new IllegalArgumentException("No ResourceBundle is supplied"));
     }
 
-    default String displayNameFull() {
-	return displayNameFull(Locale::getDefault);
+    default String displayNameFull(Supplier<ResourceBundle> bundleSupplier) {
+	return Optional.ofNullable(bundleSupplier.get()).flatMap(v -> {
+	    try {
+		return Optional
+			.ofNullable(v.getString(String.format("%1$s.%2$s.full", this.getClass().getName(), name())));
+	    } catch (NullPointerException | MissingResourceException | ClassCastException e) {
+		return Optional.empty();
+	    }
+	}).orElseThrow(() -> new IllegalArgumentException("No ResourceBundle is supplied"));
     }
 
-    default String displayNameShort() {
-	return displayNameShort(Locale::getDefault);
+    default String displayNameShort(Supplier<ResourceBundle> bundleSupplier) {
+	return Optional.ofNullable(bundleSupplier.get()).flatMap(v -> {
+	    try {
+		return Optional
+			.ofNullable(v.getString(String.format("%1$s.%2$s.short", this.getClass().getName(), name())));
+	    } catch (NullPointerException | MissingResourceException | ClassCastException e) {
+		return Optional.empty();
+	    }
+	}).orElseThrow(() -> new IllegalArgumentException("No ResourceBundle is supplied"));
     }
 
     //
 
     default String displayName(final Locale locale) {
-	return CachedResourceBundles
-		.byLocale(getBundleBaseName(), Objects.requireNonNull(locale, "Locale must provided")).flatMap(v -> {
-		    try {
-			return Optional
-				.ofNullable(v.getString(String.format("%1$s.%2$s", this.getClass().getName(), name())));
-		    } catch (NullPointerException | MissingResourceException | ClassCastException e) {
-			return Optional.empty();
-		    }
-		}).orElseThrow(IllegalArgumentException::new);
+	return displayName(() -> CachedResourceBundles.getCached(getBundleBaseName(), locale));
     }
 
     default String displayNameFull(final Locale locale) {
-	return CachedResourceBundles
-		.byLocale(getBundleBaseName(), Objects.requireNonNull(locale, "Locale must provided")).flatMap(v -> {
-		    try {
-			return Optional
-				.ofNullable(v
-					.getString(String.format("%1$s.%2$s.full", this.getClass().getName(), name())));
-		    } catch (NullPointerException | MissingResourceException | ClassCastException e) {
-			return Optional.ofNullable(displayName(locale));
-		    }
-		}).orElseThrow(IllegalArgumentException::new);
+	return displayNameFull(() -> CachedResourceBundles.getCached(getBundleBaseName(), locale));
     }
 
     default String displayNameShort(final Locale locale) {
-	return CachedResourceBundles
-		.byLocale(getBundleBaseName(), Objects.requireNonNull(locale, "Locale must provided")).flatMap(v -> {
-		    try {
-			return Optional
-				.ofNullable(v.getString(
-					String.format("%1$s.%2$s.short", this.getClass().getName(), name())));
-		    } catch (Exception e) {
-			return Optional.ofNullable(displayName(locale));
-		    }
-		}).orElseThrow(IllegalArgumentException::new);
+	return displayNameShort(() -> CachedResourceBundles.getCached(getBundleBaseName(), locale));
     }
 
     //
@@ -95,18 +90,18 @@ public interface LocalizedElement {
 
 	private static final ConcurrentMap<String, ConcurrentMap<Locale, ResourceBundle>> CACHE = new ConcurrentHashMap<>();
 
-	private static Optional<ResourceBundle> byLocale(final String baseName, final Locale locale) {
-	    return Optional.ofNullable(
-		    CACHE //
-			    .computeIfAbsent(baseName, x -> new ConcurrentHashMap<>()) //
-			    .computeIfAbsent(locale, x -> {
-				try {
-				    return ResourceBundle.getBundle(baseName, x);
-				} catch (NullPointerException | MissingResourceException e) {
-				    return null;
-				}
-			    }) //
-	    );
+	private static ResourceBundle getCached(final String baseName, final Locale locale) {
+	    return CACHE //
+		    .computeIfAbsent(Objects.requireNonNull(baseName, "Basename must be provided"),
+			    x -> new ConcurrentHashMap<>()) //
+		    .computeIfAbsent(Objects.requireNonNull(locale, "Locale must be provided"), x -> {
+			try {
+			    return ResourceBundle.getBundle(baseName, x);
+			} catch (NullPointerException | MissingResourceException e) {
+			    return null;
+			}
+		    }) //
+	    ;
 	}
     }
 
