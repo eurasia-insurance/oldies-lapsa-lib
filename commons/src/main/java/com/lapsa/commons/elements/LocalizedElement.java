514b559364d0f0c5b28932cae68d1d369342b661
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-09-23 15:49:26 +0600

    LocalizedElement splitted on two. For Enums and for other POJOs

--- commons/src/main/java/com/lapsa/commons/elements/LocalizedElement.java
+++ commons/src/main/java/com/lapsa/commons/elements/LocalizedElement.java
@@ -1,28 +1,21 @@
 package com.lapsa.commons.elements;
 
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.function.Function;
 import java.util.stream.Stream;
 import java.util.stream.Stream.Builder;
 
-import com.lapsa.commons.function.Predicates;
+import com.lapsa.commons.function.MyCollectors;
+import com.lapsa.commons.function.MyMaps;
+import com.lapsa.commons.function.MyPredicates;
 
-public interface LocalizedElement {
-
-    public static enum DisplayNameVariant {
-	NORMAL("%1$s.%2$s"), FULL("%1$s.%2$s.full"), SHORT("%1$s.%2$s.short");
-
-	private final Function<LocalizedElement, String> BUNDLE_KEY_RESOLVER;
-
-	private DisplayNameVariant(String keyPatern) {
-	    this.BUNDLE_KEY_RESOLVER = x -> String.format(keyPatern, x.getClass().getName(), x.name());
-	}
-    }
+public interface LocalizedElement extends Localized {
 
     //
 
@@ -38,36 +31,26 @@ public interface LocalizedElement {
 
     //
 
-    default String displayName(DisplayNameVariant variant) {
-	return displayName(variant, Locale.getDefault());
-    }
+    @Override
+    default String displayName(final DisplayNameVariant variant, final Locale locale) {
+	Objects.requireNonNull(variant, "Display name variant must be provided");
+	Objects.requireNonNull(locale, "Locale must be provided");
 
-    default String displayName(DisplayNameVariant variant, final Locale locale) {
-	ResourceBundle b = CachedResourceBundles.getCached(getBundleBaseName(), locale);
-	return displayName(variant, b);
-    }
-
-    default String displayName(DisplayNameVariant variant, ResourceBundle bundle) {
+	Objects.requireNonNull(getBundleBaseName(), "Bunddle basename can not be null");
 
 	Builder<DisplayNameVariant> builder = Stream.<DisplayNameVariant> builder() //
 		.add(variant);
 	switch (variant) {
 	case FULL:
 	case SHORT:
-	    builder.add(DisplayNameVariant.NORMAL);
+	    builder.accept(DisplayNameVariant.NORMAL);
 	    break;
 	default:
 	}
-	
+
 	return builder.build() //
-		.map(x -> {
-		    try {
-			return bundle.getString(x.BUNDLE_KEY_RESOLVER.apply(this));
-		    } catch (NullPointerException | MissingResourceException | ClassCastException e) {
-			return null;
-		    }
-		}) //
-		.filter(Predicates.objectNotNull()) //
+		.map(x -> ResourceBundleUtil.getLocalized(this, x, locale)) //
+		.filter(MyPredicates.objectNotNull()) //
 		.findFirst()
 		.orElseThrow(() -> new IllegalArgumentException("No ResourceBundle is supplied or key "
 			+ "is not mapped correctly"));
@@ -75,65 +58,55 @@ public interface LocalizedElement {
 
     //
 
-    default String displayName() {
-	return displayName(DisplayNameVariant.NORMAL, Locale.getDefault());
-    }
+    static class ResourceBundleUtil {
 
-    default String displayNameFull() {
-	return displayName(DisplayNameVariant.FULL, Locale.getDefault());
-    }
+	private ResourceBundleUtil() {
+	}
 
-    default String displayNameShort() {
-	return displayName(DisplayNameVariant.SHORT, Locale.getDefault());
-    }
+	//
 
-    //
+	private static Map<DisplayNameVariant, String> KEY_PATTERNS = Arrays.asList( //
+		MyMaps.entry(DisplayNameVariant.FULL, "%1$s.%2$s.full"), //
+		MyMaps.entry(DisplayNameVariant.SHORT, "%1$s.%2$s.short"), //
+		MyMaps.entry(DisplayNameVariant.NORMAL, "%1$s.%2$s") //
+	).stream().collect(MyCollectors.unmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
 
-    default String displayName(final Locale locale) {
-	return displayName(DisplayNameVariant.NORMAL, locale);
-    }
+	private static <T extends LocalizedElement> String getResourceKey(final T entity,
+		final DisplayNameVariant variant) {
+	    return String.format(ResourceBundleUtil.KEY_PATTERNS.get(variant), //
+		    entity.getClass().getName(), //
+		    entity.name());
+	}
 
-    default String displayNameFull(final Locale locale) {
-	return displayName(DisplayNameVariant.FULL, locale);
-    }
-
-    default String displayNameShort(final Locale locale) {
-	return displayName(DisplayNameVariant.SHORT, locale);
-    }
-
-    //
-
-    default String displayName(ResourceBundle bundle) {
-	return displayName(DisplayNameVariant.NORMAL, bundle); //
-    }
-
-    default String displayNameFull(ResourceBundle bundle) {
-	return displayName(DisplayNameVariant.FULL, bundle); //
-    }
-
-    default String displayNameShort(ResourceBundle bundle) {
-	return displayName(DisplayNameVariant.SHORT, bundle); //
-    }
-
-    //
-
-    static class CachedResourceBundles {
+	//
 
 	private static final ConcurrentMap<String, ConcurrentMap<Locale, ResourceBundle>> CACHE = new ConcurrentHashMap<>();
 
-	private static ResourceBundle getCached(final String baseName, final Locale locale) {
+	private static <T extends LocalizedElement> ResourceBundle getCachedResourceBundle(final T entity,
+		final Locale locale) {
 	    return CACHE //
-		    .computeIfAbsent(Objects.requireNonNull(baseName, "Basename must be provided"),
-			    x -> new ConcurrentHashMap<>()) //
-		    .computeIfAbsent(Objects.requireNonNull(locale, "Locale must be provided"), x -> {
+		    .computeIfAbsent(entity.getBundleBaseName(), x -> new ConcurrentHashMap<>()) //
+		    .computeIfAbsent(locale, x -> {
 			try {
-			    return ResourceBundle.getBundle(baseName, x);
+			    return ResourceBundle.getBundle(entity.getBundleBaseName(), x);
 			} catch (NullPointerException | MissingResourceException e) {
 			    return null;
 			}
 		    }) //
 	    ;
 	}
-    }
 
+	//
+
+	private static <T extends LocalizedElement> String getLocalized(final T entity,
+		final DisplayNameVariant variant, final Locale locale) {
+	    ResourceBundle bundle = getCachedResourceBundle(entity, locale);
+	    String key = getResourceKey(entity, variant);
+	    try {
+		return bundle.getString(key);
+	    } catch (Exception e) {
+		return null;
+	    }
+	}
+    }
 }
